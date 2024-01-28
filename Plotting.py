42b2784e8f1c20b1a408f58e9083079f7421d0a3
adleefuad <lleeffuad@gmail.com>
2024-01-29 00:39:15 +0800

    Add Question, Correction in Visualisation, Create py file

---
+++ Plotting.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.ticker as ticker
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt 
+import geopandas as gpd
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+
+#get data and plot then save
---
+++ main.py
@@ -0,0 +1,7 @@
+import scheduler
+import import_export
+import plotting
+
+#import data
+#plot and save
+#export to telegram
